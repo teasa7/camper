08c4dfe9d78f4d570af6b5e5f48b15341316da8b
Анастасия Чиняева <nastya.chinyaeva@gmail.com>
2023-05-16 01:59:04 +0500

    Add block footer

--- js/main.js
+++ js/main.js
@@ -7,3 +7,22 @@ const picker = new easepick.create({
     ],
     format: 'DD.MM.YY',
 });
+
+// Subscribe Label
+const inputSubscribe = document.querySelector('.subscribe__input');
+const label = document.querySelector('.subscribe__label');
+var regexp = /^(([^<>()[\].,;:\s@"]+(\.[^<>()[\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].,;:\s@"]+\.)+[^<>()[\].,;:\s@"]{2,})$/iu
+inputSubscribe.addEventListener('input', ()=> {
+    if(inputSubscribe.value.trim() !== '') {
+        label.classList.add('subscribe__label--top');
+        label.classList.remove('subscribe__label--invalid');
+        if(regexp.test(inputSubscribe.value) == false) {
+            label.classList.add('subscribe__label--invalid');
+        }
+        else label.classList.remove('subscribe__label--invalid');
+    }
+    else {
+        label.classList.remove('subscribe__label--top');
+        label.classList.remove('subscribe__label--invalid');
+    }
+})
